7fd9741fdb7a165a4b78b17447a61121dca9e2a7
Paul Ajibola <ife.jibola@gmail.com>
2021-07-27 01:18:22 -0400

    deployment

--- server/server.js
+++ server/server.js
@@ -14,15 +14,7 @@ import articlesRoutes from './routes/article.routes'
 
 import { matchRoutes } from 'react-router-config'
 import MainRouter from '../client/MainRouter'
-// var assert = require('assert');
 
-// describe('Array', function () {
-//     describe('#indexOf()', function () {
-//         it('should return -1 when the value is not present', function () {
-//             assert.equal([1, 2, 3].indexOf(4), -1);
-//         });
-//     });
-// });
 //Express 
 const server = express();
 
